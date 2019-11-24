cfda0132a2c1b4d93b77f00a8eff46b41d59cdf2
Dsullivan06 <57306844+Dsullivan06@users.noreply.github.com>
2019-11-24 13:03:51 -0600

    Update Character.java

--- Character.java
+++ Character.java
@@ -2,6 +2,9 @@ public abstract class Character {
     String name;
     int health;
     int attack;
+    
+    Character(){
+    }
 
     @Override
     public String toString() {
@@ -10,5 +13,29 @@ public abstract class Character {
                 ", health=" + health +
                 ", attack=" + attack +
                 '}';
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+    
+    public int getAttack(){
+        return attack;
+    }
+    
+    public void setName(String x){
+        name = x;
+    }
+    
+    public void setHealth(int x){
+        health = x;
+    }
+    
+    public void setAttack(int x){
+        attack = x;
     }
 }
